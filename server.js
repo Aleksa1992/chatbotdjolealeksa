124bcada7fc035dc4d624ecb9a94b69f79b1290b
Aleksa1992 <aleksa1992@hotmail.com>
2017-02-09 15:13:28 +0100

    test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('1728627823')
 })
 
 // for Facebook verification
@@ -30,3 +30,38 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+const token = "EAAZAN6FtJJVsBAMFsl4RaqnZBZAZBZByjG9Kz5LlLqDKPcvTGTBux9c55se4dnr9zsyUGZCNdRWU2ESwrxSh0kmN9WUxNJhQ8Pk2OlyOACk7IGNY3jeN2maLQOl1oinKVSSV2E5WndQgQdSFTJJlb1n8UdFOAJkZCkgy2OBbVUFqAZDZD";
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Desi Djole pickin dime ko ti dade takvo ime: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200);
+})
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
